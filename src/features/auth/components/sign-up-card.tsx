5a00502ee94b87d5a527831d1209e6063b04f598
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-09-12 21:50:07 -0600

    enable email/password authentication and register process

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -6,15 +6,48 @@ import { Separator } from "@/components/ui/separator"
 import { FaGithub } from "react-icons/fa"
 import { FcGoogle } from "react-icons/fc"
 import { SignInFlow } from "../types"
+import { TriangleAlert } from "lucide-react"
+import { useAuthActions } from "@convex-dev/auth/react"
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void
 }
 
 export const SignUpCard = ({ setState }: SignUpCardProps) => {
+  const { signIn } = useAuthActions()
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("")
+  const [pending, setPending] = useState(false)
+  const [error, setError] = useState("")
+
+  const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (password !== confirmPassword) {
+      setError("Password do not match")
+      return;
+    }
+
+    setPending(true)
+    signIn("password", { email, password, flow: "signUp" })
+      .catch((e) => {
+        console.log("e", e.message)
+        setError("Something went wrong")
+      })
+      .finally(() => {
+        setPending(false)
+      })
+
+
+  }
+
+  const onProviderSignUp = (value: "github" | "google") => {
+    console.log('sign in.....')
+    signIn(value).then(() => {
+      setPending(false);
+    })
+  }
 
   return (
     <Card className="w-full h-full p-8">
@@ -26,10 +59,16 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
           Use your email or another service to continue
         </CardDescription>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form className="space-y-2.5" onSubmit={onPasswordSignUp}>
           <Input
-            disabled={false}
+            disabled={pending}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
@@ -37,7 +76,7 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
@@ -45,22 +84,22 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             placeholder="Confirm password"
             type="password"
             required
           />
-          <Button type="submit" className="w-full" size="lg" disabled={false}>
+          <Button type="submit" className="w-full" size="lg" disabled={pending}>
             Continue
           </Button>
         </form>
         <Separator />
         <div className="flex flex-col gap-y-2.5">
           <Button
-            disabled={false}
-            onClick={() => { }}
+            disabled={pending}
+            onClick={() => onProviderSignUp("google")}
             variant="outline"
             size="lg"
             className="w-full relative"
@@ -69,8 +108,8 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
             Continue with Google
           </Button>
           <Button
-            disabled={false}
-            onClick={() => { }}
+            disabled={pending}
+            onClick={() => onProviderSignUp("github")}
             variant="outline"
             size="lg"
             className="w-full relative"

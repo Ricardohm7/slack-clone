79c9ba7de0d525f38228b4a7b16e1c7fc8c564e5
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-09-12 18:45:44 -0600

    complete github authentication

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import { AuthScreen } from '@/features/auth/components/auth-screen'
 
 const AuthPage = () => {
-  return (
-    <div>
-      Logged In
-    </div>
-  )
+  return (<AuthScreen />)
 }
 
 export default AuthPage
